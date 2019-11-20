db4929c4ca94f45fafe7e5c6818a8a65d0c72eb2
aoxiang <axlighting@163.com>
2019-11-21 01:26:22 +0800

    2019-11-21
    凌晨1:26

---
+++ blog-project/blog-website/README.md
@@ -0,0 +1,16 @@
+# blog-website
+
+## 项目配置
+
+### prettier
+
+```
+{
+	"singleQuote": true,
+	// 箭头函数参数括号 默认avoid 可选 avoid| always
+	// avoid 能省略括号的时候就省略 例如x => x
+	// always 总是有括号
+	"arrowParens": "always"
+}
+
+```
---
+++ blog-project/blog-website/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>
+      前端页面展示
+    </h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {}
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+// #app {
+// }
+</style>
